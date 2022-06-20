cbca59a615ca57d69bc3df846dcd8de7e519df48
Varacolacci <robe.slack@gmail.com>
2022-06-19 21:03:01 -0600

    First checkin from Rider

---
+++ LearningCode/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var numbersList = new List<int>()
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+};
+
+foreach (var number in numbersList)
+{
+    Console.WriteLine($"Number: {number}");
+}
